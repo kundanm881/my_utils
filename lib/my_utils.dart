ac9e294e4c3b0391e58cc743432ff886ffebc0cb
Kundan mandal <45583046+kundanm881@users.noreply.github.com>
2023-03-23 10:36:30 +0530

    setup for first release 2

--- lib/my_utils.dart
+++ lib/my_utils.dart
@@ -1,5 +1,9 @@
-library my_utils;
+// library my_utils;
+// export 'package:my_utils/my_utils.dart';
+// export 'k_size.dart';
 
-export 'k_size.dart';
-
-
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
