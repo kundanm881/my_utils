2e4a1fb137e91d86007f775c9815470bb391b802
Kundan mandal <45583046+kundanm881@users.noreply.github.com>
2023-03-23 10:14:53 +0530

    test

--- lib/k_size.dart
+++ lib/k_size.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 extension Number on num {
   ///  [num] is the value type [this] take value of [num]  1.
+  ///  this is test
   Widget get height => SizedBox(height: double.parse("$this"));
   Widget get width => SizedBox(width: double.parse("$this"));
 }
