4041a1756ea23850f21bf8c42101453ab040dc7c
Kundan mandal <45583046+kundanm881@users.noreply.github.com>
2023-05-19 15:54:16 +0530

    enum

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kd_utils/kd_utils.dart';
+import 'ui/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,57 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            200.height,
-
-            10.width,
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
--- lib/kd_utils.dart
+++ lib/kd_utils.dart
@@ -4,3 +4,4 @@ export 'extensions/size.dart';
 export 'extensions/context_extension.dart';
 // api services
 export 'api_service/api_expo.dart';
+
